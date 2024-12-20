70dde1f8ad041b7c83e7b2f5f7c717c6440d70a9
Janelle <jhouston@signs365.com>
2024-12-20 11:42:28 -0500

    updated Carter from being in BuilderBlocks to the Catalog.API proj since it cannot be configured in DI

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container before building the application
+// Dependencay Injection Configuration
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
