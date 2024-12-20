50f22972d2b3aed3bf6359e9735a1f94eb1b5d91
Janelle <jhouston@signs365.com>
2024-12-20 10:52:03 -0500

    Developed POST endpoint using carter and mapster and created a global usings file to avoid repeating code portions

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using Carter;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
     public record CreateProductResponse(Guid Id);
@@ -8,7 +6,26 @@ namespace Catalog.API.Products.CreateProduct
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            throw new NotImplementedException();
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                // Mapster Adapt method takes generic Command Obj(CreateProductCommand) and convert from a request to command object
+                var command = request.Adapt<CreateProductCommand>();
+
+                // MediatR requires Command Obj to trigger Command Handler so trigger MediatR using the Sender Obj
+                var result = await sender.Send(command);
+
+                // need to convert back to CreateProductResponse
+                var response = result.Adapt<CreateProductResponse>();
+
+                //return the newly created product ID using the results class from ASP>Net
+                return Results.Created($"/products/{response.Id}", response);
+            })
+                // Carter library extension methods to provide ways to configure out HTTP method
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
         }
     }
 }
