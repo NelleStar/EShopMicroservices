e2030556d84299d7044e2b5b13b9af0de4f71f56
Janelle <jhouston@signs365.com>
2024-12-20 10:12:26 -0500

    Implement ICommand into vertical slice handler, install Carter and Mapster and prep POST endpoint to use

--- src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -2,7 +2,8 @@
 
 namespace BuildingBlocks.CQRS
 {
-    public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    public interface IQueryHandler<in TQuery, TResponse> 
+        : IRequestHandler<TQuery, TResponse>
         where TQuery : IQuery<TResponse>
         where TResponse : notnull
     {
