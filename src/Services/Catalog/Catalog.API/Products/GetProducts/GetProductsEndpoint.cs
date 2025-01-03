fffa56f883cbc7a3a1e55fae9704d7b5ffe13c7b
Janelle <jhouston@signs365.com>
2025-01-03 14:20:40 -0500

    created GET products and POST product

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    // best practice - always define request and response in the endpoint
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                // Get all products
+                var result = await sender.Send(new GetProductsQuery());
+
+                // after response convert type from mediatR handle class to a getproductresponse obj
+                var response = result.Adapt<GetProductsResponse>();
+
+                // return the response
+                return Results.Ok(result);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    // best practice - always define query/command and result in the Handler
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+
+    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) 
+        : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+            
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            
+            return new GetProductsResult(products);
+        }
+    }
+}
