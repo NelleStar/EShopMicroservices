ad35418a71ce8d8f5e2f4610a4d012db23e4d7cb
Janelle <jhouston@signs365.com>
2025-01-07 09:02:42 -0500

    added seeding data

--- src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -2,41 +2,30 @@
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
-namespace BuildingBlocks.Behaviors
+namespace BuildingBlocks.Behaviors;
+public class LoggingBehavior<TRequest, TResponse>
+    (ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull, IRequest<TResponse>
+    where TResponse : notnull
 {
-    public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger) 
-        : IPipelineBehavior<TRequest, TResponse>
-        where TRequest : notnull, IRequest<TRequest>
-        where TResponse : notnull       
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
-        {
-            logger.LogInformation("[START] Handle request={Request} - Response={Response} - RequestData={RequestData", 
-                typeof(TRequest).Name, 
-                typeof(TResponse).Name, 
-                request
-            );
+        logger.LogInformation("[START] Handle request={Request} - Response={Response} - RequestData={RequestData}",
+            typeof(TRequest).Name, typeof(TResponse).Name, request);
 
-            var timer = new Stopwatch();
+        var timer = new Stopwatch();
+        timer.Start();
 
-            timer.Start();
-            var response = await next();
-            timer.Stop();
-            
-            var timeTaken = timer.Elapsed;
-            if(timeTaken.Seconds >3)
-            {
-                logger.LogWarning("[PERFORMANCE] The request {Request} took {TimeTaken} seconds",
-                    typeof(TRequest).Name,
-                    timeTaken.Seconds
-                );
-            }
+        var response = await next();
 
-            logger.LogInformation("[END] Handled {Request} with {Response}",
-                typeof(TRequest).Name,
-                typeof(TResponse).Name
-            );
-            return response;
-        }
+        timer.Stop();
+        var timeTaken = timer.Elapsed;
+        if (timeTaken.Seconds > 3) // if the request is greater than 3 seconds, then log the warnings
+            logger.LogWarning("[PERFORMANCE] The request {Request} took {TimeTaken} seconds.",
+                typeof(TRequest).Name, timeTaken.Seconds);
+
+        logger.LogInformation("[END] Handled {Request} with {Response}", typeof(TRequest).Name, typeof(TResponse).Name);
+        return response;
     }
 }
