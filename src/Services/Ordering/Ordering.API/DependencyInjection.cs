e507f9f647621a28186582d42648651147cf08c3
Janelle <jhouston@signs365.com>
2025-01-22 10:25:13 -0500

    updated classes to be extension methods for DI

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiService(this IServiceCollection services)
+    {
+        //services.AddCarter();
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        //app.MapCarter();
+        return app;
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,16 @@
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+
+/* 
+Infrastructure - EF Core
+Application - MediatR
+API - Carter, HealthChecks, ...
+ */
+builder.Services.AddApplicationServices().AddInfrastructureServices(builder.Configuration).AddWebServices();
 
 var app = builder.Build();
 
